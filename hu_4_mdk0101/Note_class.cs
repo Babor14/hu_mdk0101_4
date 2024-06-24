59915da7a7ef70c343d19a15eabcff02e19b944e
user9 <user9@mail.ru>
2024-06-24 17:12:02 +0200

    Добавьте файлы проекта.

---
+++ hu_4_mdk0101/Note_class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hu_4_mdk0101;
+
+namespace hu_4_mdk0101
+{
+    public class Note_class: ICloneable, IComparable<Note_class>
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int[] Birthdate { get; set; }
+        public object Clone() 
+        {
+            return this.MemberwiseClone();
+        }
+        public int CompareTo(Note_class other)
+        {
+            return string.Compare(this.PhoneNumber.Substring(0, 3),
+                other.PhoneNumber.Substring(0, 3));
+        }
+    }
+    
+}
+public class LastNameComparer : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        Note_class note1 = (Note_class)x;
+        Note_class note2 = (Note_class)y;
+        return string.Compare(note1.LastName + note1.FirstName, note2.LastName + note2.FirstName);
+    }
+}
---
+++ hu_4_mdk0101/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using hu_4_mdk0101;
+
+Note_class[] notes= new Note_class[2];
+for(int i = 0; i < 8; i++)
+{
+    notes[i]=new Note_class();
+    Console.WriteLine("Фамилия:");
+    notes[i].LastName= Console.ReadLine();
+    Console.WriteLine("Имя:");
+    notes[i].FirstName = Console.ReadLine();
+    Console.WriteLine("Номер телефона:");
+    notes[i].PhoneNumber = Console.ReadLine();
+    notes[i].Birthdate = new int[3];
+    Console.WriteLine("Дата рождения (день,месяц,год)");
+        for(int j = 0; j<3; j++) 
+    {
+        notes[i].Birthdate[j]=int.Parse(Console.ReadLine());
+    }
+}
+Array.Sort(notes);
+Console.WriteLine("Введите фамилию:");
+string searchLastName=Console.ReadLine();
+bool found = false;
+foreach(var note in notes)
+{
+    if (note.LastName == searchLastName)
+    {
+        Console.WriteLine($"Имя:{note.FirstName}");
+        Console.WriteLine($"Фамилия{note.LastName}");
+        Console.WriteLine($"Телефон{note.PhoneNumber}");
+        Console.WriteLine($"дата рождения{note.Birthdate}");
+        found = true;
+        break;
+    }
+}
+if(!found)
+{
+    Console.WriteLine("No marching record found");
+}
+
